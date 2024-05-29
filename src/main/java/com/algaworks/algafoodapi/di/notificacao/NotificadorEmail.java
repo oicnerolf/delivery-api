0c7c582f6cdf1ad5fa1e33ffb22bf0717871458c
Gabriel Oliveira <gabriel.pb.congo@gmail.com>
2024-05-29 10:03:40 -0300

    definindo beans

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,12 +1,13 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NotificadorEmail {
 
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do e-mail %s: %s\n",
-                cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 
 }
--- src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -2,7 +2,9 @@ package com.algaworks.algafoodapi.di.service;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
 import com.algaworks.algafoodapi.di.notificacao.NotificadorEmail;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
     private NotificadorEmail notificador;
